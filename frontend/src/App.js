2e6f2189fb1910df0da133a0afb93b82dfc93452
Harshareddy433 <155610226+Reddy433@users.noreply.github.com>
2024-08-14 21:51:29 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,25 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
-import AuthForm from "./components/AuthForm";
+import LoginPage from "./pages/LoginPage";
+import Dashboard from "./pages/Dashboard";
 import CompanyForm from "./components/CompanyForm";
 import EmployeeForm from "./components/EmployeeForm";
 import EmployeeList from "./components/EmployeeList";
+import EmployeeDetail from "./pages/EmployeeDetail";
 
 const App = () => {
   return (
     <Router>
       <Header />
-      <main className="container mx-auto p-4">
-        <Routes>
-          <Route path="/login" element={<AuthForm />} />
-          <Route path="/companies/create" element={<CompanyForm />} />
-          <Route path="/employees/create" element={<EmployeeForm />} />
-          <Route path="/" element={<EmployeeList />} />
-        </Routes>
-      </main>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/companies/create" element={<CompanyForm />} />
+        <Route path="/employees/create" element={<EmployeeForm />} />
+        <Route path="/employees" element={<EmployeeList />} />
+        <Route path="/employees/:id" element={<EmployeeDetail />} />
+      </Routes>
     </Router>
   );
 };
